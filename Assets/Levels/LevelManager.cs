90eff618df3bf1dba46cb49f554b581f0bafd9e0
itsMatoosh <matirebacz@op.pl>
2017-03-02 15:49:23 +0100

    Finished the Level Loader.

--- Assets/Levels/LevelManager.cs
+++ Assets/Levels/LevelManager.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 	/// <summary>
 	/// List of all the available normal levels.
 	/// </summary>
-	public string[] normalLevels;
+	public static string[] normalLevels;
 	/// <summary>
 	/// List of all the available tie breaker levels.
 	/// </summary>
-	public string[] tieBreakerLevels;
+	public static string[] tieBreakerLevels;
 
 	/// <summary>
 	/// Loads a random level.
 	/// </summary>
 	/// <param name="tieBreaker">If set to <c>true</c> will load a tiebreaker level.</param>
 	/// <param name="blackList">Levels not to load.</param>
-	public void LoadLevel(bool tieBreaker, List<string> blackList) {
+	public static void LoadLevel(bool tieBreaker, List<string> blackList) {
 		string chosenLevel = "";
 
 		while (chosenLevel == "") {
+			if (normalLevels.Length == 0 || tieBreakerLevels.Length == 0) {
+				Debug.LogError ("Not enough levels to play the game! :(");
+				break;
+			}
+
 			if (tieBreaker) {
 				chosenLevel = tieBreakerLevels[Random.Range(0, tieBreakerLevels.Length - 1)];
 			} else {
@@ -29,6 +35,10 @@ public class LevelManager : MonoBehaviour {
 			if (blackList.Contains (chosenLevel)) {
 				chosenLevel = "";
 			}
+		}
+
+		if (chosenLevel != "") {
+			SceneManager.LoadSceneAsync (chosenLevel, LoadSceneMode.Additive);
 		}
 	}
 }
