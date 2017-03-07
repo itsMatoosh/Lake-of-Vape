be1515c9cb25ca09ba068e4ff58d3d06f4176d62
itsMatoosh <matirebacz@op.pl>
2017-03-07 13:25:30 +0100

    Match button added

--- Assets/Levels/Menus/Lobby/LobbyManager.cs
+++ Assets/Levels/Menus/Lobby/LobbyManager.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking.Match;
 
 public class LobbyManager : MonoBehaviour {
+	/// <summary>
+	/// The instance.
+	/// </summary>
+	public static LobbyManager instance;
+
+	/// <summary>
+	/// The match button prefab.
+	/// </summary>
+	public Object matchButtonPrefab;
+
 	void Start() {
 		//Listing all the active games.
 		//TODO: Game name filtering.
+		instance = this;
 		RefreshMatches();
 	}
 	/// <summary>
@@ -21,5 +33,12 @@ public class LobbyManager : MonoBehaviour {
 	/// </summary>
 	public void CreateMatch() {
 		SceneManager.LoadScene ("CreateMatch");
+	}
+	/// <summary>
+	/// Adds the match button.
+	/// </summary>
+	/// <param name="info">Info.</param>
+	public void AddMatchButton(MatchInfo info) {
+
 	}
 }
