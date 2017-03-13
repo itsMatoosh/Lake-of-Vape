d892f50faf395af61023ac2ded7d030f04913a3c
itsMatoosh <matirebacz@op.pl>
2017-03-13 10:06:28 +0100

    Fixxxxxxxx

--- Assets/Actors/Player/PlayerMovement.cs
+++ Assets/Actors/Player/PlayerMovement.cs
@@ -4,58 +4,58 @@ using UnityEngine.Networking;
 
 public class PlayerMovement : NetworkBehaviour {
 
-    /// <summary>
-    /// Rigidbody of this player.
-    /// </summary>
-    public Rigidbody2D body;
+	/// <summary>
+	/// Rigidbody of this player.
+	/// </summary>
+	public Rigidbody2D body;
 
-    /// <summary>
-    /// The speed of the player.
-    /// </summary>
-    public float speed;
+	/// <summary>
+	/// The speed of the player.
+	/// </summary>
+	public float speed;
 
-    /// <summary>
-    /// Input of the player.
-    /// </summary>
-    public class PlayerInput
-    {
-        /// <summary>
-        /// Cache of X input.
-        /// </summary>
-        public float inputXCache;
-        /// <summary>
-        /// Cache of Y input.
-        /// </summary>
-        public float inputYCache;
+	/// <summary>
+	/// Input of the player.
+	/// </summary>
+	public class PlayerInput
+	{
+		/// <summary>
+		/// Cache of X input.
+		/// </summary>
+		public float inputXCache;
+		/// <summary>
+		/// Cache of Y input.
+		/// </summary>
+		public float inputYCache;
 
-        /// <summary>
-        /// Timestamp of the input.
-        /// </summary>
-        public double timeStamp;
-    }
-    /// <summary>
-    /// Used to verify the position predicted by client.
-    /// </summary>
-    public class Result {
-        /// <summary>
-        /// Server side player x position.
-        /// </summary>
-        public float posX;
-        /// <summary>
-        /// Server side player y position.
-        /// </summary>
-        public float posY;
+		/// <summary>
+		/// Timestamp of the input.
+		/// </summary>
+		public double timeStamp;
+	}
+	/// <summary>
+	/// Used to verify the position predicted by client.
+	/// </summary>
+	public class Result {
+		/// <summary>
+		/// Server side player x position.
+		/// </summary>
+		public float posX;
+		/// <summary>
+		/// Server side player y position.
+		/// </summary>
+		public float posY;
 
-        /// <summary>
-        /// Timestamp of the results.
-        /// </summary>
-        public double timeStamp;
-    }
-    /// <summary>
-    /// Cache of the input before fixed update.
+		/// <summary>
+		/// Timestamp of the results.
+		/// </summary>
+		public double timeStamp;
+	}
+	/// <summary>
+	/// Cache of the input before fixed update.
 	/// Client side
-    /// </summary>
-    public PlayerInput clientInputCache = new PlayerInput();
+	/// </summary>
+	public PlayerInput clientInputCache = new PlayerInput();
 	/// <summary>
 	/// The last client input.
 	/// </summary>
@@ -65,127 +65,140 @@ public class PlayerMovement : NetworkBehaviour {
 	/// Server side.
 	/// </summary>
 	public PlayerInput serverInputCache = new PlayerInput();
-    /// <summary>
-    /// Cache of the server result.
-    /// </summary>
-    public Result resultCache = null;
-    /// <summary>
-    /// Result cache of the client.
-    /// </summary>
-    public List<Result> clientResults = new List<Result>();
+	/// <summary>
+	/// Cache of the server result.
+	/// </summary>
+	public Result resultCache = null;
+	/// <summary>
+	/// Result cache of the client.
+	/// </summary>
+	public List<Result> clientResults = new List<Result>();
 
-    /// <summary>
-    /// Calculating the movement.
-    /// </summary>
-    private void FixedUpdate()
-    {
-        if (isServer) {
-            //Sending predictions for the last fixedupodate simulations.
-            if(resultCache != null)
-            {
-                resultCache.posX = transform.position.x;
-                resultCache.posY = transform.position.y;
-                RpcOnResultsReceived(resultCache);
-            }
+	public override void OnStartClient ()
+	{
+		if (!isLocalPlayer) {
+			body.isKinematic = false;
+		}
+	}
 
-            //Simulating on...
+	/// <summary>
+	/// Calculating the movement.
+	/// </summary>
+	private void FixedUpdate()
+	{
+		if (isServer) {
+			//Sending predictions for the last fixedupodate simulations.
+			if(resultCache != null)
+			{
+				resultCache.posX = transform.position.x;
+				resultCache.posY = transform.position.y;
+				RpcOnResultsReceived(resultCache);
+			}
+
+			//Simulating on...
 			body.velocity = new Vector2(serverInputCache.inputXCache * speed,
 				serverInputCache.inputYCache * speed);
 
-            //Resetting the input and waiting for input from player.
-            resultCache = new Result
-            {
+			//Resetting the input and waiting for input from player.
+			resultCache = new Result
+			{
 				timeStamp = serverInputCache.timeStamp
-            };
+			};
 			//serverInputCache = new PlayerInput();
-        }
-        if (isLocalPlayer)
-        {
-            //Adding client results to the cache.
-            clientResults.Add(new Result
-            {
-                posX = transform.position.x,
-                posY = transform.position.y,
-				timeStamp = lastClientInput.timeStamp
-            });
-            if(clientResults.Count > 15)
-            {
-                clientResults.RemoveAt(0);
-            }
+		}
+		if (isLocalPlayer)
+		{
+			//Adding client results to the cache.
+			clientResults.Add(new Result
+				{
+					posX = transform.position.x,
+					posY = transform.position.y,
+					timeStamp = lastClientInput.timeStamp
+				});
+			if(clientResults.Count > 15)
+			{
+				clientResults.RemoveAt(0);
+			}
 
-            //Sending input update.
+			//Sending input update.
 			clientInputCache.timeStamp = Time.timeSinceLevelLoad;
 			CmdSendInput(clientInputCache);
 			lastClientInput.timeStamp = clientInputCache.timeStamp;
 
-            //Simulating locally.
+			//Simulating locally.
 			body.velocity = new Vector2(clientInputCache.inputXCache * speed,
 				clientInputCache.inputYCache * speed);
 
 			//Resetting the input.
 			clientInputCache = new PlayerInput();
-        }
-    }
+		}
+	}
 
-    /// <summary>
-    /// Getting input.
-    /// </summary>
-    private void Update()
-    {
-        if (!isLocalPlayer) return;
-        if(Input.GetAxis("Horizontal") != 0)
-        {
+	/// <summary>
+	/// Getting input.
+	/// </summary>
+	private void Update()
+	{
+		if (!isLocalPlayer) return;
+		if(Input.GetAxis("Horizontal") != 0)
+		{
 			clientInputCache.inputXCache = Input.GetAxis("Horizontal");
-        }
+		}
 
-        if(Input.GetAxis("Vertical") != 0)
-        {
+		if(Input.GetAxis("Vertical") != 0)
+		{
 			clientInputCache.inputYCache = Input.GetAxis("Vertical");
-        }
-    }
+		}
+	}
 
-    /// <summary>
-    /// Sends input info to the server.
-    /// </summary>
-    /// <param name="input"></param>
-    [Command]
-    public void CmdSendInput(PlayerInput input)
-    {
-        //Called on the server.
+	/// <summary>
+	/// Sends input info to the server.
+	/// </summary>
+	/// <param name="input"></param>
+	[Command]
+	public void CmdSendInput(PlayerInput input)
+	{
+		//Called on the server.
 		serverInputCache = input;
-    }
-    /// <summary>
-    /// Called when the server sends the simulation results.
-    /// </summary>
-    /// <param name="result"></param>
-    [ClientRpc]
-    public void RpcOnResultsReceived(Result result)
-    {
+	}
+	/// <summary>
+	/// Called when the server sends the simulation results.
+	/// </summary>
+	/// <param name="result"></param>
+	[ClientRpc]
+	public void RpcOnResultsReceived(Result result)
+	{
 		if (!isLocalPlayer) {
-			transform.position.Set (result.posX, result.posY, 1);
+			if (isServer) {
+				return;
+			}
+			body.MovePosition (new Vector2(result.posX, result.posY));
 			return;
 		}
 
-        //Checking if the results match between client and the server.
-        Result matchingClientResult = null;
-		for (int i = 0; i < clientResults.Count; i++) {
-			if(clientResults[i].timeStamp == result.timeStamp)
+		if (!isServer) {
+			//Checking if the results match between client and the server.
+			Result matchingClientResult = null;
+			for (int i = 0; i < clientResults.Count; i++) {
+				if(clientResults[i].timeStamp == result.timeStamp)
+				{
+					matchingClientResult = clientResults[i-1];
+				}
+			}
+
+			if(matchingClientResult == null)
 			{
-				matchingClientResult = clientResults[i+1];
+				Debug.LogError("No matching client simulation samples! ");
+				return;
+			}
+
+			if(result.posX != matchingClientResult.posX || result.posY != matchingClientResult.posY)
+			{
+				Debug.LogError("SERVER-CLIENT MISMATCH!!!");
+				Debug.Log("Server: " + result.posX + "/ " + result.timeStamp + " Client: " + matchingClientResult.posX + "/" + matchingClientResult.timeStamp);
+				transform.position.Set (result.posX, result.posY, 1);
+				body.MovePosition (new Vector2(result.posX, result.posY));
 			}
 		}
-	
-        if(matchingClientResult == null)
-        {
-			Debug.LogError("No matching client simulation samples! ");
-            return;
-        }
-
-        if(result.posX != matchingClientResult.posX || result.posY != matchingClientResult.posY)
-        {
-            Debug.LogError("SERVER-CLIENT MISMATCH!!!");
-            Debug.Log("Server: " + result.posX + "/ " + result.timeStamp + " Client: " + matchingClientResult.posX + "/" + matchingClientResult.timeStamp);
-			transform.position.Set (result.posX, result.posY, 1);
-        }
-    }
+	}
 }
